f836288962670a55913d39bcf95209d9e26970ab
Vasil Stoyanov <vasil.stoyanov@feg.bg>
2018-08-20 15:39:46 +0200

    use apollo to make requests in the fronend, tests not working

--- server/app.js
+++ server/app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
+//allow cross-origin request
+app.use(cors());
+
+//connect to local database
 mongoose.connect("mongodb://localhost:27017/graphql", {useNewUrlParser: true});
 mongoose.connection.once('open', () => {
     console.log("Connected to local MongoDB");
